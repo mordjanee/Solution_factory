5c62ee874bb335ff0688d77d138097485a729310
mordjanee <mordjane.aribi@efrei.net>
2023-06-25 13:04:50 +0200

    knn

--- Main.py
+++ Main.py
@@ -37,58 +37,77 @@ plt.show()
 #%%Création du dictionnaire et de la liste avec les mots les plus fréquents
 
 from collections import Counter
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
 
 sentences_text = "".join(df["Text"].str.lower())
 mots = sentences_text.split()
-mots_frequents = Counter(mots).most_common(10000)
+mots_frequents = Counter(mots).most_common(2000)
 
 dictionnaire = {indice : mot for indice, (mot, _) in enumerate(mots_frequents)}
 liste = [mot for mot,_ in mots_frequents]
 print(liste)
 
-def contain(mot):
-    for i in liste :
-        if i == mot :
+def contain(word, sentence):
+    for i in sentence.split() :
+        if i == word :
             return True
     return False
 
-def encoding(sentence):
+def encoding(sentence, liste):
     lst = []
-    for i in sentence.split() :
-        if (contain(i)):
+    for i in liste :
+        if (contain(i, sentence)):
             lst.append(1)
         else :
             lst.append(0)
     return lst
 
             
-df["Encoding"] = df["Text"].apply(lambda x: encoding(x))
+df["Encoding"] = df["Text"].apply(lambda x: encoding(x, liste))
 
 print(len(df.loc[0, "Encoding"]))
 
-
-
-#%%
-
-from sklearn import neighbors, datasets, preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-
-X = df.Text
+X = df.Encoding.tolist()
 y = df.Score
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
 
+
+#%%
+
+from sklearn import neighbors
+
+
 knn = neighbors.KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
 y_pred = knn.predict(X_test)
-accuracy_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred)
+print("KNN accuracy:", accuracy)
 
 
+#%%
+
+from sklearn.svm import SVC
+
+svc = SVC(kernel = "linear")
+svc.fit(X_train, y_train)
+y_pred = svc.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("SVC accuracy:", accuracy)
 
 
+#%% Sauvegarde du modèle
 
+import pickle
 
+# Sauvegarde du modèle
+with open('modele_knn_sauvegarde.pkl', 'wb') as fichier:
+    pickle.dump(knn, fichier)
 
-
+# Chargement du modèle
+with open('modele_knn_sauvegarde.pkl', 'rb') as fichier:
+    knn_sauvegarde = pickle.load(fichier)
 
